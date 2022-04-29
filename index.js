bc6e0403a24cb57b9dd512d8fbf45e746535a45c
rakibul114 <rakibulislam114@gmail.com>
2022-04-29 17:19:56 +0600

    jwt token implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,16 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verify JWT Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401);
+    }
+    console.log('Inside verifyJWT', authHeader);
+    next();
+}
 
 // for connecting to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aehxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -25,6 +36,15 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
         const orderCollection = client.db('geniusCar').collection('order');
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        });
         
         // GET API for all services
         app.get('/service', async (req, res) => {
@@ -60,7 +80,7 @@ async function run() {
 
         // Order collection API
         // GET API
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {           
             const email = req.query.email;
             const query = {email: email};
             const cursor = orderCollection.find(query);
